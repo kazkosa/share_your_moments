18b41e3d350839146641ebe98a413f0b546a2d6b
kazkosa <kazu_kosa@icloud.com>
2019-10-01 18:05:23 +0900

    update routes to add likes routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
-  resources :users, only: [:edit, :update]
-  resources :posts, only: [:index, :new, :create, :edit, :update, :destroy, :show] do
+  resources :users,   only: [:edit, :update]
+  resources :posts,   only: [:index, :new, :create, :edit, :update, :destroy, :show] do
     resources :comments, only: [:new, :create]
+    resources :likes,    only: [:create,:destroy]
   end
 end
