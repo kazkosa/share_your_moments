a8dda08613fae992ac7333ec9c84f78c09015f16
kazkosa <kazu_kosa@icloud.com>
2019-10-02 05:11:43 +0900

    Update model post,user and Create favorite

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments,:dependent => :delete_all
-  has_many :likes,:dependent => :delete_all
+  has_many :comments,:dependent   => :delete_all
+  has_many :likes,:dependent      => :delete_all
+  has_many :favorites,:dependent  => :delete_all
   has_many :post_tags
   has_many :tags, through: :post_tags
   validates :title, presence: true
