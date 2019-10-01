c8548bb0f7146bbc312add6337e89da94ec8ec7d
kazkosa <kazu_kosa@icloud.com>
2019-10-01 18:03:53 +0900

    Update date base to create the likes table

---
+++ db/migrate/20191001004632_create_likes.rb
@@ -0,0 +1,9 @@
+class CreateLikes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :likes do |t|
+      t.references :user, foreign_key: true, on_delete: :cascade
+      t.references :post, foreign_key: true, on_delete: :cascade
+      t.timestamps
+    end
+  end
+end
