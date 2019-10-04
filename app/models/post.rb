e686eed654d198d94d0330b22672582961a99cba
kazkosa <kazu_kosa@icloud.com>
2019-10-04 19:40:29 +0900

    Modify tag function

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   has_many :comments,:dependent   => :delete_all
   has_many :likes,:dependent      => :delete_all
   has_many :favorites,:dependent  => :delete_all
-  has_many :post_tags
+  has_many :post_tags ,:dependent => :delete_all
   has_many :tags, through: :post_tags
   validates :title, presence: true
   validates :content, presence: true
