a5d94e04f466bd8f30cfb650ec193809ff78af6c
kazkosa <kazu_kosa@icloud.com>
2019-10-10 09:58:51 +0900

    Create follower and following tab

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -16,4 +16,80 @@ $(function(){
     readURL(this);
 
   })
+
+
+  $(".user-posts-btn").on("click",function(e){
+    e.preventDefault();
+    $(".user-posts-btn"     ).removeClass("btn-outline-secondary ");
+    $(".user-posts-btn"     ).addClass("btn-secondary ");
+    $(".user-favorites-btn" ).removeClass("btn-secondary ");
+    $(".user-favorites-btn" ).addClass("btn-outline-secondary ");
+    $(".user-followers-btn" ).removeClass("btn-secondary ");
+    $(".user-followers-btn" ).addClass("btn-outline-secondary ");
+    $(".user-followings-btn").removeClass("btn-secondary ");
+    $(".user-followings-btn").addClass("btn-outline-secondary ");
+
+    $(".user-post-main__inner__container__list__posts").show(200);
+    $(".user-post-main__inner__container__list__favorites").hide(200);
+    $(".user-post-main__inner__container__list__followers").hide(200);
+    $(".user-post-main__inner__container__list__followings").hide(200);
+  });
+
+
+  $(".user-favorites-btn").on("click",function(e){
+    e.preventDefault();
+    $(".user-posts-btn"     ).removeClass("btn-secondary ");
+    $(".user-posts-btn"     ).addClass("btn-outline-secondary ");
+    $(".user-favorites-btn" ).removeClass("btn-outline-secondary ");
+    $(".user-favorites-btn" ).addClass("btn-secondary ");
+    $(".user-followers-btn" ).removeClass("btn-secondary ");
+    $(".user-followers-btn" ).addClass("btn-outline-secondary ");
+    $(".user-followings-btn").removeClass("btn-secondary ");
+    $(".user-followings-btn").addClass("btn-outline-secondary ");
+
+    $(".user-post-main__inner__container__list__posts").hide(200);
+    $(".user-post-main__inner__container__list__favorites").show(200);
+    $(".user-post-main__inner__container__list__followers").hide(200);
+    $(".user-post-main__inner__container__list__followings").hide(200);
+
+
+  });
+
+  $(".user-followers-btn").on("click",function(e){
+    e.preventDefault();
+    $(".user-posts-btn"     ).removeClass("btn-secondary ");
+    $(".user-posts-btn"     ).addClass("btn-outline-secondary ");
+    $(".user-favorites-btn" ).removeClass("btn-secondary ");
+    $(".user-favorites-btn" ).addClass("btn-outline-secondary ");
+    $(".user-followers-btn" ).removeClass("btn-outline-secondary ");
+    $(".user-followers-btn" ).addClass("btn-secondary ");
+    $(".user-followings-btn").removeClass("btn-secondary ");
+    $(".user-followings-btn").addClass("btn-outline-secondary ");
+
+    $(".user-post-main__inner__container__list__posts").hide(200);
+    $(".user-post-main__inner__container__list__favorites").hide(200);
+    $(".user-post-main__inner__container__list__followers").show(200);
+    $(".user-post-main__inner__container__list__followings").hide(200);
+
+
+  });
+
+  $(".user-followings-btn").on("click",function(e){
+    e.preventDefault();
+    $(".user-posts-btn"     ).removeClass("btn-secondary ");
+    $(".user-posts-btn"     ).addClass("btn-outline-secondary ");
+    $(".user-favorites-btn" ).removeClass("btn-secondary ");
+    $(".user-favorites-btn" ).addClass("btn-outline-secondary ");
+    $(".user-followers-btn" ).removeClass("btn-secondary ");
+    $(".user-followers-btn" ).addClass("btn-outline-secondary ");
+    $(".user-followings-btn").removeClass("btn-outline-secondary ");
+    $(".user-followings-btn").addClass("btn-secondary ");
+
+    $(".user-post-main__inner__container__list__posts").hide(200);
+    $(".user-post-main__inner__container__list__favorites").hide(200);
+    $(".user-post-main__inner__container__list__followers").hide(200);
+    $(".user-post-main__inner__container__list__followings").show(200);
+
+  });
+
 })
