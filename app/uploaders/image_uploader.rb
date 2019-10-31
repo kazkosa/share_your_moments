307fad8fbfdab436106183b6c900b012020d5eb8
kazkosa <kazu_kosa@icloud.com>
2019-11-01 06:27:27 +0900

    Update deploy setting

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,9 +5,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   # process resize_to_limit: [1200, 1200]
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
-  process quality: 100
+  if Rails.env.development? || Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
+
+  # process quality: 100
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
